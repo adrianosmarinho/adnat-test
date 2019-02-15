1ea7cc1ac4917ae4caa31688375cd6cd6e4456a5
adrianosmarinho <adrianosmarinho@gmail.com>
2019-02-15 18:37:20 +1000

    Finish static pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,12 @@
 class StaticPagesController < ApplicationController
   def home
+    #executes the code and then renders the view
+    #because there is no code, it just renders the view
   end
 
   def help
+  end
+
+  def about
   end
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,12 +2,18 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
+    #Here the use of get indicates that our tests expect the Home and Help pages to be ordinary web pages, accessed using a GET reques
     get static_pages_home_url
     assert_response :success
   end
 
   test "should get help" do
     get static_pages_help_url
+    assert_response :success
+  end
+
+  test "should get about" do
+    get static_pages_about_url
     assert_response :success
   end
 
